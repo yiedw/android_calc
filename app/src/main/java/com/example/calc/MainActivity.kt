94091fae6b8c32e1b9acc4fee04a978e0a76d748
yiedw <yiedw@naver.com>
2020-03-16 13:11:59 +0900

    button color

--- app/src/main/java/com/example/calc/MainActivity.kt
+++ app/src/main/java/com/example/calc/MainActivity.kt
@@ -6,12 +6,13 @@ import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.system.exitProcess
 
 class MainActivity : AppCompatActivity() {
 
     var condition: Int = 0
     var hidden_number: Double = 0.0
-    var zero_check :Boolean=false
+    var zero_check: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,16 +22,17 @@ class MainActivity : AppCompatActivity() {
     fun onClick(view: View) {
         var number = findViewById(R.id.TextView_number) as TextView
         var temp: String = number.text.toString()
+        if(temp.equals("Error"))
+            temp="0"
         var temp_number: Double = temp.toDouble()
 
         var check: Boolean = false
         when (view.id) {
             R.id.one -> {
                 if (temp.equals("0") || zero_check) {
-                    zero_check=false
+                    zero_check = false
                     temp = "1"
-                }
-                else if (temp.length == 2 && temp.first() == '-' && temp.get(1) == '0') {
+                } else if (temp.length == 2 && temp.first() == '-' && temp.get(1) == '0') {
                     temp = "-1"
                 } else
                     temp += "1"
@@ -39,9 +41,8 @@ class MainActivity : AppCompatActivity() {
             R.id.two -> {
                 if (temp.equals("0") || zero_check) {
                     temp = "2"
-                    zero_check=false
-                }
-                else if (temp.length == 2 && temp.first() == '-' && temp.get(1) == '0') {
+                    zero_check = false
+                } else if (temp.length == 2 && temp.first() == '-' && temp.get(1) == '0') {
                     temp = "-2"
                 } else
                     temp += "2"
@@ -49,9 +50,8 @@ class MainActivity : AppCompatActivity() {
             R.id.three -> {
                 if (temp.equals("0") || zero_check) {
                     temp = "3"
-                    zero_check=false
-                }
-                else if (temp.length == 2 && temp.first() == '-' && temp.get(1) == '0') {
+                    zero_check = false
+                } else if (temp.length == 2 && temp.first() == '-' && temp.get(1) == '0') {
                     temp = "-3"
                 } else
                     temp += "3"
@@ -60,8 +60,7 @@ class MainActivity : AppCompatActivity() {
                 if (temp.equals("0") || zero_check) {
                     temp = "4"
                     zero_check = false
-                }
-                else if (temp.length == 2 && temp.first() == '-' && temp.get(1) == '0') {
+                } else if (temp.length == 2 && temp.first() == '-' && temp.get(1) == '0') {
                     temp = "-4"
                 } else
                     temp += "4"
@@ -70,8 +69,7 @@ class MainActivity : AppCompatActivity() {
                 if (temp.equals("0") || zero_check) {
                     temp = "5"
                     zero_check = false
-                }
-                else if (temp.length == 2 && temp.first() == '-' && temp.get(1) == '0') {
+                } else if (temp.length == 2 && temp.first() == '-' && temp.get(1) == '0') {
                     temp = "-5"
                 } else
                     temp += "5"
@@ -80,8 +78,7 @@ class MainActivity : AppCompatActivity() {
                 if (temp.equals("0") || zero_check) {
                     temp = "6"
                     zero_check = false
-                }
-                else if (temp.length == 2 && temp.first() == '-' && temp.get(1) == '0') {
+                } else if (temp.length == 2 && temp.first() == '-' && temp.get(1) == '0') {
                     temp = "-6"
                 } else
                     temp += "6"
@@ -91,8 +88,7 @@ class MainActivity : AppCompatActivity() {
                 if (temp.equals("0") || zero_check) {
                     temp = "7"
                     zero_check = false
-                }
-                else if (temp.length == 2 && temp.first() == '-' && temp.get(1) == '0') {
+                } else if (temp.length == 2 && temp.first() == '-' && temp.get(1) == '0') {
                     temp = "-7"
                 } else
                     temp += "7"
@@ -101,8 +97,7 @@ class MainActivity : AppCompatActivity() {
                 if (temp.equals("0") || zero_check) {
                     temp = "8"
                     zero_check = false
-                }
-                else if (temp.length == 2 && temp.first() == '-' && temp.get(1) == '0') {
+                } else if (temp.length == 2 && temp.first() == '-' && temp.get(1) == '0') {
                     temp = "-8"
                 } else
                     temp += "8"
@@ -111,8 +106,7 @@ class MainActivity : AppCompatActivity() {
                 if (temp.equals("0") || zero_check) {
                     temp = "9"
                     zero_check = false
-                }
-                else if (temp.length == 2 && temp.first() == '-' && temp.get(1) == '0') {
+                } else if (temp.length == 2 && temp.first() == '-' && temp.get(1) == '0') {
                     temp = "-9"
                 } else
                     temp += "9"
@@ -121,8 +115,7 @@ class MainActivity : AppCompatActivity() {
                 if (temp.equals("0") || zero_check) {
                     temp = "0"
                     zero_check = false
-                }
-                else if (temp.length == 2 && temp.first() == '-' && temp.get(1) == '0') {
+                } else if (temp.length == 2 && temp.first() == '-' && temp.get(1) == '0') {
                     temp = "-0"
                 } else
                     temp += "0"
@@ -134,27 +127,27 @@ class MainActivity : AppCompatActivity() {
                 condition = 1
             }
             R.id.minus -> {
-                check=true
+                check = true
                 hidden_number = decide(condition, temp_number, hidden_number)
                 condition = 2
             }
             R.id.mul -> {
-                check=true
+                check = true
                 hidden_number = decide(condition, temp_number, hidden_number)
                 condition = 3
             }
             R.id.div -> {
-                check=true
+                check = true
                 hidden_number = decide(condition, temp_number, hidden_number)
                 condition = 4
             }
             R.id.rest -> {
-                check=true
+                check = true
                 hidden_number = decide(condition, temp_number, hidden_number)
                 condition = 5
             }
             R.id.end -> {
-                check=true
+                check = true
                 hidden_number = decide(condition, temp_number, hidden_number)
                 condition = 0
             }
@@ -179,22 +172,37 @@ class MainActivity : AppCompatActivity() {
         }
         if (!check) {
 //            TextView_number.textSize=1
-            if(temp.length>9)
+
+
+            if (temp.length > 9)
                 TextView_number.setTextSize(40f)
-            else if(temp.length>6)
+            else if (temp.length > 6)
                 TextView_number.setTextSize(70f)
-            TextView_number.setText(temp)
-        }
-        else{
-            zero_check=true
-            if(hidden_number.toString().length>9)
-                TextView_number.setTextSize(40f)
-            else if(hidden_number.toString().length>6)
-                TextView_number.setTextSize(70f)
-            if (hidden_number % 1.0 != 0.0)
-                TextView_number.setText(hidden_number.toString())
             else
-                TextView_number.setText(hidden_number.toLong().toString())
+                TextView_number.setTextSize(100f)
+            if (temp.length > 18)
+                TextView_number.setText("Error")
+            else
+
+                TextView_number.setText(temp)
+        } else {
+            zero_check = true
+            if (hidden_number.toString().length > 18)
+                TextView_number.setText("Error")
+            if (hidden_number.toString().length > 9)
+                TextView_number.setTextSize(40f)
+            else if (hidden_number.toString().length > 6)
+                TextView_number.setTextSize(70f)
+            else
+                TextView_number.setTextSize(100f)
+            if (hidden_number.toString().length > 18)
+                TextView_number.setText("Error")
+            else {
+                if (hidden_number % 1.0 != 0.0)
+                    TextView_number.setText(hidden_number.toString())
+                else
+                    TextView_number.setText(hidden_number.toLong().toString())
+            }
         }
 
     }
